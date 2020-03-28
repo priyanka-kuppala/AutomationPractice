9ae176c463097c4313aba0b15c10f095aae8e2fb
priyanka <priyanka@priyanka>
2020-03-28 18:24:30 +0530

    Project Commit

--- reports/report.js
+++ reports/report.js
@@ -35,7 +35,8 @@ formatter.match({
   "location": "com.cts.stepdefinitions.StepDefinition.i_enter_login_details_from_Excel_with_SheetName(java.lang.String,java.lang.String)"
 });
 formatter.result({
-  "status": "passed"
+  "error_message": "org.openqa.selenium.NoSuchWindowException: no such window: target window already closed\nfrom unknown error: web view not found\n  (Session info: chrome\u003d80.0.3987.149)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027PRIYANKA\u0027, ip: \u0027192.168.43.15\u0027, os.name: \u0027Windows 8.1\u0027, os.arch: \u0027amd64\u0027, os.version: \u00276.3\u0027, java.version: \u002713.0.2\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 80.0.3987.149, chrome: {chromedriverVersion: 79.0.3945.36 (3582db32b3389..., userDataDir: C:\\Users\\Admin\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:55766}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: ce548ed99f643d9f949d21e382ed1542\n*** Element info: {Using\u003dxpath, value\u003d//a[text()\u003d\u0027My Account\u0027]}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat com.cts.pages.MyAccount.clickOnMyAccount(MyAccount.java:18)\r\n\tat com.cts.stepdefinitions.StepDefinition.i_enter_login_details_from_Excel_with_SheetName(StepDefinition.java:77)\r\n\tat ✽.I enter login details from Excel \"src/test/resources/resources/Book1.xlsx\" with SheetName \"LoginCredentials\"(file:///C:/priyanka/PracticeAutomation/PracticeAutomation/src/main/resources/feature/AutomationPractice.feature:10)\r\n",
+  "status": "failed"
 });
 formatter.step({
   "name": "click on login button",
@@ -45,7 +46,7 @@ formatter.match({
   "location": "com.cts.stepdefinitions.StepDefinition.click_on_login_button()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "I click on shop",
@@ -55,7 +56,7 @@ formatter.match({
   "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_shop()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "I select a book",
@@ -65,7 +66,7 @@ formatter.match({
   "location": "com.cts.stepdefinitions.StepDefinition.i_select_a_book()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "I click on the add to basket button",
@@ -75,7 +76,7 @@ formatter.match({
   "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_the_add_to_basket_button()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.step({
   "name": "Item should be added to the view cart",
@@ -85,401 +86,9 @@ formatter.match({
   "location": "com.cts.stepdefinitions.StepDefinition.item_should_be_added_to_the_view_cart()"
 });
 formatter.result({
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User launch the browser with practiceautomationtestingPage",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.user_launch_the_browser_with_practiceautomationtestingPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Add to cart and Remove",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "name": "I click on login button and enter loginusername as \u0027pavanapriyanaka56@gmail.com\u0027 and I enter loginpassword as \u0027Ramadhiganesh@park12K6\u0027",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_login_button_and_enter_loginusername_as_and_I_enter_loginpassword_as(java.lang.String,java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "click on login button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.click_on_login_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on shop",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_shop()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I select a book",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_select_a_book()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on the add to basket button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_the_add_to_basket_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I delete from cart",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_delete_from_cart()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "Item should be removed from the view cart",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.item_should_be_removed_from_the_view_cart()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User launch the browser with practiceautomationtestingPage",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.user_launch_the_browser_with_practiceautomationtestingPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Add to cart and Proceed to checkout",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "name": "I click on login button and enter loginusername as \u0027pavanapriyanaka56@gmail.com\u0027 and I enter loginpassword as \u0027Ramadhiganesh@park12K6\u0027",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_login_button_and_enter_loginusername_as_and_I_enter_loginpassword_as(java.lang.String,java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "click on login button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.click_on_login_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on shop",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_shop()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I select a book",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_select_a_book()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on proceed to checkout",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_proceed_to_checkout()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "It should navigate to the next page",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.it_should_navigate_to_the_next_page()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User launch the browser with practiceautomationtestingPage",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.user_launch_the_browser_with_practiceautomationtestingPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "Billing details",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "name": "I click on login button and enter loginusername as \u0027pavanapriyanaka56@gmail.com\u0027 and I enter loginpassword as \u0027Ramadhiganesh@park12K6\u0027",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_login_button_and_enter_loginusername_as_and_I_enter_loginpassword_as(java.lang.String,java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "click on login button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.click_on_login_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on shop",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_shop()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I select a book",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_select_a_book()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on proceed to checkout",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_proceed_to_checkout()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "It should navigate to the billing page with details",
-  "rows": [
-    {},
-    {}
-  ],
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.it_should_navigate_to_the_billing_page_with_details(io.cucumber.datatable.DataTable)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I should get filled form",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_should_get_filled_form()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
-formatter.background({
-  "name": "",
-  "description": "",
-  "keyword": "Background"
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "User launch the browser with practiceautomationtestingPage",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.user_launch_the_browser_with_practiceautomationtestingPage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.scenario({
-  "name": "place order",
-  "description": "",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "name": "I click on login button and enter loginusername as \u0027pavanapriyanaka56@gmail.com\u0027 and I enter loginpassword as \u0027Ramadhiganesh@park12K6\u0027",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_login_button_and_enter_loginusername_as_and_I_enter_loginpassword_as(java.lang.String,java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "click on login button",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.click_on_login_button()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on shop",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_shop()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I select a book",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_select_a_book()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on proceed to checkout",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_proceed_to_checkout()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "It should navigate to the billing page with details",
-  "rows": [
-    {},
-    {}
-  ],
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.it_should_navigate_to_the_billing_page_with_details(io.cucumber.datatable.DataTable)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I click on the place order",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_click_on_the_place_order()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "I should get the message as orderplaced",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "com.cts.stepdefinitions.StepDefinition.i_should_get_the_message_as_orderplaced()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.after({
-  "status": "passed"
-});
+  "error_message": "org.openqa.selenium.NoSuchWindowException: no such window: target window already closed\nfrom unknown error: web view not found\n  (Session info: chrome\u003d80.0.3987.149)\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:17:03\u0027\nSystem info: host: \u0027PRIYANKA\u0027, ip: \u0027192.168.43.15\u0027, os.name: \u0027Windows 8.1\u0027, os.arch: \u0027amd64\u0027, os.version: \u00276.3\u0027, java.version: \u002713.0.2\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 80.0.3987.149, chrome: {chromedriverVersion: 79.0.3945.36 (3582db32b3389..., userDataDir: C:\\Users\\Admin\\AppData\\Loca...}, goog:chromeOptions: {debuggerAddress: localhost:55766}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: ce548ed99f643d9f949d21e382ed1542\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:609)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.getScreenshotAs(RemoteWebDriver.java:295)\r\n\tat com.cts.stepdefinitions.StepDefinition.end(StepDefinition.java:54)\r\n",
+  "status": "failed"
 });
